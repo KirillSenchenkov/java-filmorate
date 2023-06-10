efceb071c7d92f54b64b7d6e8d39f1b0d572982a
KirillSenchenkov <heikoo59@gmail.com>
2023-06-10 14:12:45 +0300

    Скорректировать стиль написания кода

--- src/test/java/ru/yandex/practicum/filmorate/validator/ValidatorTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/validator/ValidatorTest.java
@@ -20,8 +20,9 @@ class ValidatorTest {
     public static FilmController filmController;
     public User user = new User(1, "sunbaked@list.ru", "Maikoo", "Heikoo",
             LocalDate.of(1991, 11, 18));
-    public Film film = new Film(1,"Film", "Film",
+    public Film film = new Film(1, "Film", "Film",
             LocalDate.of(2000, 11, 11), 100);
+
     @BeforeAll
     public static void beforeAll() {
         userController = new UserController();
@@ -29,7 +30,7 @@ class ValidatorTest {
     }
 
     @Test
-    void shouldThrownExceptionForIncorrectUSerEmail(){
+    void shouldThrownExceptionForIncorrectUSerEmail() {
         user.setEmail("");
         exception = assertThrows(ValidationException.class, (() -> userController.createUser(user)));
         assertEquals("Email адрес указан не верно", exception.getMessage());
@@ -42,7 +43,7 @@ class ValidatorTest {
     }
 
     @Test
-    void shouldThrownExceptionForIncorrectUserLogin(){
+    void shouldThrownExceptionForIncorrectUserLogin() {
         user.setLogin("");
         exception = assertThrows(ValidationException.class, (() -> userController.createUser(user)));
         assertEquals("Логин не может быть пустым или содержать в себе пробелы", exception.getMessage());
@@ -55,17 +56,17 @@ class ValidatorTest {
     }
 
     @Test
-    void shouldThrownExceptionForIncorrectUserBirthDate(){
+    void shouldThrownExceptionForIncorrectUserBirthDate() {
         user.setBirthday(null);
         exception = assertThrows(ValidationException.class, (() -> userController.createUser(user)));
         assertEquals("Не указана дата рождения", exception.getMessage());
-        user.setBirthday(LocalDate.of(2100, 11,11));
+        user.setBirthday(LocalDate.of(2100, 11, 11));
         exception = assertThrows(ValidationException.class, (() -> userController.createUser(user)));
         assertEquals("Дата рождения не может быть в будущем", exception.getMessage());
     }
 
     @Test
-    void shouldThrownExceptionForIncorrectFilmName(){
+    void shouldThrownExceptionForIncorrectFilmName() {
         film.setName("");
         exception = assertThrows(ValidationException.class, (() -> filmController.createFilm(film)));
         assertEquals("Название фильма не может быть пустым", exception.getMessage());
@@ -75,7 +76,7 @@ class ValidatorTest {
     }
 
     @Test
-    void shouldThrownExceptionForIncorrectFilmReleaseDate(){
+    void shouldThrownExceptionForIncorrectFilmReleaseDate() {
         film.setReleaseDate(null);
         exception = assertThrows(ValidationException.class, (() -> filmController.createFilm(film)));
         assertEquals("Дата создания не может пустой", exception.getMessage());
@@ -85,14 +86,14 @@ class ValidatorTest {
     }
 
     @Test
-    void shouldThrownExceptionForIncorrectFilmDescription(){
+    void shouldThrownExceptionForIncorrectFilmDescription() {
         film.setDescription("a".repeat(201));
         exception = assertThrows(ValidationException.class, (() -> filmController.createFilm(film)));
         assertEquals("Описание не может превышать 200 символов", exception.getMessage());
     }
 
     @Test
-    void shouldThrownExceptionForIncorrectFilmDuration(){
+    void shouldThrownExceptionForIncorrectFilmDuration() {
         film.setDuration(-1);
         exception = assertThrows(ValidationException.class, (() -> filmController.createFilm(film)));
         assertEquals("Продолжительность фильма не может быть отрицательной", exception.getMessage());
